3e8afde4a8dd602b903e58df842da27a298e3190
Nivetha1509 <nivethas120@gmail.com>
2021-09-09 14:38:19 +0530

    adding test files

---
+++ src/main/java/sample/test.java
@@ -0,0 +1,14 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+//import org.testng.annotations.Test;
+public class test {
+
+@Test
+public void testing()
+{
+	System.out.print("Hello");
+}
+
+}
